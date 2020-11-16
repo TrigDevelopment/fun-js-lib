73ae66f452d3858689e679aec2e8d0f72eb35379
ArtemkaKokorin <xx.artem-kokorin@mail.ru>
2020-11-16 14:12:08 +0300

    Add htmlTagged, htmlNew

---
+++ fun-js-lib/html.js
@@ -0,0 +1,14 @@
+/**
+ * Returns array of elements with given `tag` from `container`
+ * @type {<K extends keyof HTMLElementTagNameMap>(container: HTMLElement, tag: K) => HTMLElementTagNameMap[K][]}
+ */
+export function htmlTagged (container, tag) {
+  return Array.from(container.getElementsByTagName(tag))
+}
+
+/**
+ * @type {<K extends keyof HTMLElementTagNameMap>(tag: K) => HTMLElementTagNameMap[K]}
+ */
+export function htmlNew (tag) {
+  return document.createElement(tag)
+}
