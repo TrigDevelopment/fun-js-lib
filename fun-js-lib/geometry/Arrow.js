6adf6c566ba2f4c65b0f9b79d266e84ccec6f62e
ArtemkaKokorin <xx.artem-kokorin@mail.ru>
2021-01-22 15:43:31 +0300

    Add Arrow methods
    Add horizontal
    Add moved
    Add movedStart

--- fun-js-lib/geometry/Arrow.js
+++ fun-js-lib/geometry/Arrow.js
@@ -10,8 +10,47 @@ export default class Arrow {
    * @param {Dot} args.start
    * @param {Dot} args.end
    */
-  constructor(args) {
+  constructor (args) {
     this.start = args.start
     this.end = args.end
+  }
+  /**
+   * Creates new horizontal `Arrow`
+   * @param {Object} args 
+   * @param {number} args.xStart 
+   * @param {number} args.xEnd 
+   * @param {number} args.y 
+   */
+  static horizontal (args) {
+    return new Arrow({
+      start: new Dot({
+        x: args.xStart,
+        y: args.y
+      }),
+      end: new Dot({
+        x: args.xEnd,
+        y: args.y
+      })
+    })
+  }
+  /**
+   * Returns new `Arrow` moved by `dot`
+   * @param {Dot} dot 
+   */
+  moved (dot) {
+    return new Arrow({
+      start: this.start.moved(dot),
+      end: this.end.moved(dot)
+    })
+  }
+  /**
+   * Returns new `Arrow` with start moved by `dot`
+   * @param {Dot} dot 
+   */
+  movedStart (dot) {
+    return new Arrow({
+      start: this.start.moved(dot),
+      end: this.end.copy()
+    })
   }
 }
